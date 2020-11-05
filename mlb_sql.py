6dfecbb0fe190c4293d899806d40507a43087d42
jbell1991 <joseph.louis.bell@gmail.com>
2020-11-04 19:03:51 -0500

    duplicated pandas query

--- mlb_sql.py
+++ mlb_sql.py
@@ -5,8 +5,23 @@ engine = create_engine('sqlite://', echo=False)
 
 # convert pandas dataframe into SQL database
 df = pd.read_csv('/Users/josephbell/Desktop/sql-practice/mlb.csv')
+df = df.rename(columns= {'2B': 'Doubles', '3B': 'Triples'})
 df.to_sql('mlb', con=engine)
 
 # convert SQL query into pandas dataframe
-df2 = pd.read_sql("SELECT * FROM mlb LIMIT 5", con=engine)
+df2 = pd.read_sql("SELECT * FROM mlb LIMIT 5;", con=engine)
 print(df2)
+
+# equivalent to df.head() in pandas
+print(df.head())
+
+# average of stats for all players by season using pandas
+print(df.groupby('Season').mean())
+
+# average of stats for all players by season using SQL
+df3 = pd.read_sql(
+    """SELECT AVG(weight),AVG(height),AVG(G),AVG(AB),AVG(R),AVG(H),AVG(Doubles),AVG(Triples),AVG(HR),AVG(RBI),AVG(SB),AVG(BB),AVG(SO),AVG(salary),AVG(IBB),AVG(HBP),AVG(SH),AVG(SF),AVG(PA)
+       FROM mlb
+       GROUP BY Season;""", con=engine)
+print(df3)
+
